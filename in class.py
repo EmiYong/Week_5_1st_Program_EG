06f688ad76c99b166f88089d896d6eae53615d49
Emiliano Gutierrez <emiliano.gutierrez01@utrgv.edu>
2026-02-18 12:12:56 -0600

    completed in class assignment

---
+++ in class.py
@@ -0,0 +1,22 @@
+name="Emiliano"
+
+num=56
+
+print("Hello World, I'm printing text")
+
+print(name)
+
+print("my name is", name)
+
+name="Emi"
+print("My nickname is", name)
+
+print("what is your favorite animal?")
+animal=input()
+print("I didnt know your favorite animal was", animal,)
+
+if num>10:
+    print("Congrats, your number is greater than 10")
+
+else:
+    print("Sorry your number is not greater than 10")
---
+++ partner1_02-18.py
@@ -0,0 +1,35 @@
+# 1. Print a welcome message
+print("Welcome")
+
+# 2. Create a variable with your name
+name="Emiliano"
+# 3. Print the name
+print(name)
+
+# 4. Create a variable with your age
+age=19
+
+# 5. Print the age
+print(age)
+# 6. Ask the user for their favorite color
+color = input("Whats your favorite color?: ")
+# 7. Print a sentence using the color
+print("My favorite color is",color, "too!" )
+
+# 8. Create two number variables
+
+
+# 9. Add the numbers
+
+
+# 10. Print the result
+
+# 11. Check if age is greater than 18
+
+# 12. Print message if true
+# 13. Otherwise print another message
+# 14. Print message if false
+   
+
+# 15. Print a goodbye message
+
