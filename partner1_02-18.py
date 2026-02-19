7766e96980713e279e0273fbb45a5f9bcedb3100
EmiYong <havshs339@gmail.com>
2026-02-19 05:41:15 +0000

    Added completed Python practice script

--- partner1_02-18.py
+++ partner1_02-18.py
@@ -17,19 +17,23 @@ color = input("Whats your favorite color?: ")
 print("My favorite color is",color, "too!" )
 
 # 8. Create two number variables
-
+num1 = 10
+num2 = 5
 
 # 9. Add the numbers
 
+result = num1 + num2
 
 # 10. Print the result
-
+print("The sum is:", result)
 # 11. Check if age is greater than 18
-
+if age > 18:
 # 12. Print message if true
+    print("You are an adult.")
 # 13. Otherwise print another message
+else:
 # 14. Print message if false
-   
+   print("You are not an adult yet.")
 
 # 15. Print a goodbye message
-
+print("Goodbye!")
